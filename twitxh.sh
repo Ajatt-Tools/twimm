c8fe7366fc199efb4eed474d78505394a1ae5e1e
asakura42 <51290160+asakura42@users.noreply.github.com>
2023-12-05 20:08:02 +0000

    Update twitxh.sh

--- twitxh.sh
+++ twitxh.sh
@@ -1,213 +1,130 @@
 #!/bin/bash
 language="Español" # 日本語, Русский, English, Español...
 dir="$HOME/.local/share/twitxh"
-# instance="https://api.safetwitch.eu.projectsegfau.lt"
+instance="https://api.safetwitch.eu.projectsegfau.lt"
+#instance="https://tta.femboy.band"
 resolution="480p" # 1080p60, 720p60, 480p, 360p, 160p, audio_only
-minviewers="500"
 
-gamelist="$dir/twitchgames"
 favs="$dir/favs"
 towatch="$dir/towatch"
 watched="$dir/watched"
-blacklist="$dir/blacklist"
-backends="$dir/backends"
 streamerlist=$(mktemp)
-streams=$(mktemp)
-find_done=0
-parsing_done=0
-clips=0
 
 mkdir -p "$dir"
-sort -u "$favs" -o "$favs"
 
-getinstance() {
-
-	curl --max-time 10 -Ls https://codeberg.org/SafeTwitch/safetwitch/raw/branch/master/README.md |
-		grep "https.*|" | grep -v " ✅" | grep -o "https://[[:alnum:].\-]\+" |
-		while IFS= read -r site ; do
-			ws=$(curl --max-time 1 -Ls "$site" | grep "index.*\.js" | sed "s|\"></script>||;s|^.*\"|$site|")
-			if [ -z "$ws" ] ; then
-				continue
-			fi
-			api=$(curl --max-time 1 -Ls "$ws" | grep -o 'rootBackendUrl",`${[[:alnum:]]\+}[[:alnum:].\-]\+' |sed 's|^.*\}|https://|')
-			echo "$site $api"
-		done | awk  '(NF==2) {print $2}' | shuf > "$backends"
-
-
-	}
-
-# get instance
-
-if [ ! -f "$backends" ] ; then
-	getinstance
+if [ -z "$game_orig" ] ; then
+	game_orig="Just Chatting"
 fi
 
-instance=$(shuf -n1 "$backends")
 
 usage() {
-	echo "Usage: $(basename $0) [OPTIONS]"
-	echo "This script allows you to watch (mostly random) streams and videos from Twitch."
-	echo
-	echo "Options:"
-	echo "  -l language    Define the language (default: Español)"
-	echo "  -r resolution  Define the resolution (default: 480p)"
-	echo "  -g game        Define the game (default: asks with fzf)"
-	echo "  -c             Parse clips"
-	echo "  -w             Watch random clips from parsed list"
-	echo "  -f             Check favs' live"
-	echo "  -a             Parse streamers and add all to favs"
-	echo "  -s tag         Additional tag to search and parse"
-	echo "  -h             Display this help message"
-	echo
-	echo "Examples:"
-	echo "  $(basename $0) -l English -r 720p        Set language to English and resolution to 720p"
-	echo "  $(basename $0) -c                        Enable clips"
-	echo "  $(basename $0) -f                        Use favs"
-	echo "  $(basename $0) -a                        Add all to favs"
-	exit 1
+    echo "Usage: $0 [OPTION]..."
+    echo "Automated twitch video parser"
+    echo
+    echo "Options:"
+    echo "-l   Language of the streamer (Default: Español)"
+    echo "-r   Resolution of the video (Default: 480p)"
+    echo "-g   Game name (Default: Just Chatting)"
+    echo "-s   Tags to find"
+    echo "-a   Add tags, parse streamers and exit"
+    echo "-c   Add to watch list and exit"
+    echo "-w   Watch videos"
+    echo "-h   Display this help and exit"
+    echo "No flag will cause doing `-a' and `-c'"
+    echo
+    echo "Examples:"
+    echo "$0 -s 'Argentina'"
+    echo "    (to get watchlist of current streamers with Argentina tag)"
+    echo "$0 -l English -r 1080p60 -g 'Dota 2' -w"
+    echo "    (to watch broadcast of the Dota 2 game)"
+    exit 1
 }
 
 
-# Getting list of games
 
-gettinglist() {
-	if [ ! -f "$gamelist" ] ; then
-		echo "Loading games list..."
-		while true ; do
-			json=$(curl -Ls "$instance/api/discover?cursor=$cursor" | jq)
-			if [ $(echo "$json" | jq -r 'last(.data[].viewers)') -lt 100 ] ; then
-				break
-			fi
-			games=$(echo "$json" | jq -r '.data[].name')
-			cursor=$(echo "$json" | jq -r 'last(.data[].cursor)')
-			echo "$games" >> $gamelist
-		done
+
+addedtags() {
+	if [ -z "$addtags" ] ; then
+		addtags="$language"
 	fi
-
-	unset cursor
-
-}
-
-# Get game
-getgame() {
-
-	if [ -z "$game_orig" ] ; then
-		game_orig=$(cat "$gamelist" | fzf --height=10 --border-label="╢ Which game do you want to watch? ╟" --border=top --border-label-pos=3 --color=label:italic)
-	fi
-
-	game=$(echo "$game_orig" | sed 's/ /%20/g')
+	echo "$addtags" | sed 's/|/\n/g' | while IFS= read -r line ; do
+	curl -Ls "$instance/api/search/?query=$line" | jq -r '.data.channelsWithTag[] | select(.followers > 1000) | .username' >> "$streamerlist"
+done
 }
 
 
-# Find streamers with more than X viewers
-findstreamers () {
-	if [ "$find_done" -eq 1 ] ; then
+
+
+process_parsing() {
+	local line=$1
+	listofstreamers=$(curl -Ls "$instance/api/users/$line" | jq -r --arg jqLanguage "$language" '.data | select(.stream.tags[] | contains($jqLanguage)) | .login')
+	if [ -z "$listofstreamers" ] ; then
 		true
 	else
-		echo "Looking for streamers..."
-		while true ; do
-			json=$(curl -Ls "$instance/api/discover/$game?cursor=$cursor" | jq)
-			streamers=$(echo "$json" | jq -r '.data.streams[].streamer.name')
-			cursor=$(echo "$json" | jq -r 'last(.data.streams[].cursor)')
-			echo "$streamers" >> $streamerlist
-			if [ $(echo "$json" | jq -r "last(.data.streams[].viewers)") -lt $minviewers ] ; then
-				break
-			fi
-		done
-
-		sort -u "$streamerlist" -o "$streamerlist"
+		printf "%s " "$line"
+		echo "$listofstreamers" >> "$favs"
 	fi
-	find_done=1
+
 }
 
 parsingstreamers() {
-	if [ "$parsing_done" -eq 1 ] ; then
+	export -f process_parsing
+	export instance
+	export favs
+	export language
+	echo "Parsing streamers..."
+	xargs -P 8 -I {} bash -c 'process_parsing {}' < "$streamerlist"
+	sort -u "$favs" -o "$favs"
+
+}
+
+
+process_video() {
+	local line=$1
+	listofvids=$(curl -Ls "$instance/api/vods/shelve/$line" | jq -r '.data[] | select(.title == "All videos") | .videos[] | select(.duration > 10000) | {id: .id, game: .game.name, login: .streamer.login} | [.id, .game, .login] | @tsv')
+	if [ -z "$listofvids" ] ; then
 		true
 	else
-		echo "Parsing streamers..."
-		while read -r line ; do
-			curl -Ls "$instance/api/users/$line" | jq --arg jqLanguage "$language" '.data | select(.stream.tags[] | contains($jqLanguage)) | {login: .login, followers: .followers, title: (.stream.title | .[0:20]), viewers: .stream.viewers}' >> "$streams"
-			printf "%s " "$line"
-		done < "$streamerlist"
+		printf "%s " "$line"
+		echo "$listofvids" >> "$towatch"
 	fi
-	parsing_done=1
 }
 
 addtowatch() {
-	while read -r line ; do
-		listofvids=$(curl -Ls "$instance/api/vods/shelve/$line" | jq --arg game "$game_orig" -r '.data[] | select(.title == "All videos") | .videos[] | select(.duration > 10000) | {id: .id, game: .game.name, login: .streamer.login} | [.id, .game, .login] | @tsv')
-		if [ -z "$listofvids" ] ; then
-			continue
-		else
-			echo "$listofvids" >> "$towatch"
-		fi
-		sort -u "$towatch" -o "$towatch"
+	export -f process_video
+	export towatch
+	export instance
+	export game_orig
+	xargs -P 8 -I {} bash -c 'process_video {}' < "$favs"
+	sort -u "$towatch" -o "$towatch"
 
-
-	done <<< "$(cat "$streams" | jq -r '.login' && cat "$favs")"
 }
 
 watch() {
 	while true ; do
-		if [ "$clips" -eq 1 ] ; then
-			watchnow=$(cat "$towatch" | grep "$game_orig" | shuf -n1)
-			if [ -z "$watchnow" ] ; then
-				break
-			fi
-			if grep -q "$watchnow" "$watched" ; then
-				sed -i "/$watchnow/d" "$towatch"
-				continue
-			fi
-			video=$(echo "$watchnow" | awk -F'\t' '{print $1}')
-			streamer=$(echo "$watchnow" | awk -F'\t' '{print $NF}')
-
-			link=$(curl -Ls "$instance/proxy/vod/$video/video.m3u8"  | grep -A2 "NAME=.$resolution" | tail -n1)
-			if [ -z "$link" ] ; then
-				continue
-			fi
-			mpv --start=15:00 "$link"
-			sed -i "/^$video\t/d" "$towatch"
-			echo "$watchnow" >> "$watched"
-			echo "Ctrl-C to exit... 10 sec"
-
-		else
-			watchnow=$(cat "$streams" | jq -r '[.login, .followers, .title, .viewers] | @tsv' | column -s$'\t' -t -o' | ' | awk '{print $NF,$0}' | sort -nr | cut -f2- -d' ' | fzf --height=10 --border-label="╢ Which streamer do you want to watch? ╟" --border=top --border-label-pos=3 --color=label:italic | awk -F' | ' '{print $1}')
-			link=$(curl "$instance/proxy/stream/$watchnow/hls.m3u8" | grep -A2 "NAME=.$resolution" | tail -n1)
-			mpv "$link"
-			fav=$(printf "%s\n%s\n" "No" "Yes" | fzf --height=10 --border-label="╢ Do you want to add this streamer to favs? ╟" --border=top --border-label-pos=3 --color=label:italic)
-
-			if [ "$fav" == "Yes" ] ; then
-				echo "$watchnow" >> "$favs"
-			fi
-
-			more=$(printf "%s\n%s\n" "No" "Yes" | fzf --height=10 --border-label="╢ Do you want to watch another streamer? ╟" --border=top --border-label-pos=3 --color=label:italic)
-
-			if [ "$more" == "No" ] ; then
-				break
-			fi
+		watchnow=$(cat "$towatch" | grep "$game_orig" | shuf -n1)
+		if [ -z "$watchnow" ] ; then
+			break
 		fi
+		if grep -q "$watchnow" "$watched" ; then
+			sed -i "/$watchnow/d" "$towatch"
+			continue
+		fi
+		video=$(echo "$watchnow" | awk -F'\t' '{print $1}')
+		link=$(curl -Ls "$instance/proxy/vod/$video/video.m3u8"  | grep -A2 "NAME=.$resolution" | tail -n1)
+		if [ -z "$link" ] ; then
+			continue
+		fi
+		mpv --start=15:00 "$link"
+		sed -i "/^$video\t/d" "$towatch"
+		echo "$watchnow" >> "$watched"
+		echo "Ctrl-C to exit... 10 sec"
 
 
+		unset fav more
 
-
-
-	# streamlink -p mpv --twitch-low-latency "https://twitch.tv/$watchnow" "$resolution"
-
-
-	unset fav more
-
-	sleep 10
-done
-}
-
-addedtags() {
-	if [ -z "$addtags" ] ; then
-		true
-	else
-		echo "$addtags" | sed 's/|/\n/g' | while IFS= read -r line ; do
-		curl -Ls "$instance/api/search/?query=$line" | jq -r '.data.channelsWithTag[] | select(.followers > 1000) | .username' >> "$favs"
+		sleep 10
 	done
-	fi
 }
 
 
@@ -215,7 +132,8 @@ addedtags() {
 
 
 
-while getopts 'l:r:g:s:cwfah' OPTION; do
+
+while getopts 'l:r:g:s:cwah' OPTION; do
 	case "$OPTION" in
 		l)
 			language="$OPTARG"
@@ -230,46 +148,31 @@ while getopts 'l:r:g:s:cwfah' OPTION; do
 			addtags="$OPTARG"
 			;;
 		c)
-			clips=1
-			gettinglist ;
-			getgame ;
-			findstreamers ;
-			parsingstreamers ;
 			addtowatch ;
 			exit
 			;;
 		w)
-			clips=1
-			gettinglist ;
-			getgame ;
 			watch ;
 			exit
 			;;
 
-		f)
-			streamerlist="$favs"
-			parsingstreamers
-			watch
-			exit
-			;;
 		a)
-			gettinglist
-			getgame
-			findstreamers
-			parsingstreamers
-			cat "$streams" | jq -r '.login' >> "$favs"
 			addedtags
-			sort -u "$favs" -o "$favs"
+			parsingstreamers
 			exit
 			;;
 		h)
 			usage
+			exit
 			;;
-	esac
-done
+		*)
+			usage
+			exit
+			;;
 
-gettinglist ;
-getgame ;
-findstreamers ;
-parsingstreamers ;
-watch ;
+		esac
+	done
+
+addedtags
+parsingstreamers
+addtowatch
